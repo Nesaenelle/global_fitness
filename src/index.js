0ba2e24ee47e70fdcff189a4c765b6b73672c9ae
Bogdan Zinkevych <venomsunset@gmail.com>
2019-06-06 12:31:38 +0300

    fix

--- src/index.js
+++ src/index.js
@@ -5,3 +5,13 @@ import * as $ from 'jquery'
 $('.search-input__img').on('click', function() {
 	$(this).parent().toggleClass('active');
 });
+
+$('.main-slider__slide').hover(
+  function() {
+  	$('.main-slider__slide').removeClass( "active inactive" );
+    $( this ).addClass( "active" );
+    $( this ).prevAll().addClass( "inactive");
+  }, function() {
+    // $( this ).removeClass( "active" );
+  }
+);
