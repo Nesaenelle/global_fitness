0c3c5392dce962d17e87a3c673c876870756bfaf
Bogdan Zinkevych <venomsunset@gmail.com>
2019-06-26 22:53:07 +0300

    fix

--- src/index.js
+++ src/index.js
@@ -167,13 +167,28 @@ $('[data-dropdown-value]').on('click', function(e) {
         $('[data-dropdown]').removeClass('active');
     }
 
+    if ($(this).closest('[data-dropdown]').hasClass('dealer')) {
+        $(this).closest('[data-dropdown]').removeClass('dealer')
+        $(this).closest('[data-dropdown]').removeClass('active')
+        $(this).closest('[data-dropdown]').find('.dropdown--value').text('Выберете офис')
+        return 
+    }
+
     $(this).closest('[data-dropdown]').toggleClass('active');
 });
 
 $('[data-dropdown-item]').on('click', function(e) {
     var text = $(this).text();
-    $(this).closest('[data-dropdown]').find('[data-dropdown-value]').text(text);
-    $(this).closest('[data-dropdown]').removeClass('active');
+
+    if( $(this).data('dropdown-item') === 'dealer') {
+        e.stopPropagation();
+        var val = $(this).data('dropdown-item-value');
+        $(this).closest('[data-dropdown]').addClass('dealer')
+        $(this).closest('[data-dropdown]').find('.dropdown--value').text(val);
+    } else {    
+        $(this).closest('[data-dropdown]').find('[data-dropdown-value]').text(text);
+        $(this).closest('[data-dropdown]').removeClass('active');
+    }
 });
 
 $(window).on('click', function(e) {
